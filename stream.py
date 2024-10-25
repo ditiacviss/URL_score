deae6b289e4493246717916817bf7b4e80a5fec7
ditiacviss <diti.b@acviss.com>
2024-10-25 16:01:26 +0530

    main_change

--- stream.py
+++ stream.py
@@ -10,7 +10,8 @@ from features.get_catchingInfo_compressedinfo import check_caching_and_compressi
 import joblib
 import pickle
 import streamlit as st
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from logger.logs import logger_info
 
 model = joblib.load('ocsvm_model.h5')
 with open('scaler.pkl', 'rb') as f:
@@ -77,90 +78,22 @@ def main():
             prediction = model.predict(data_scaled)
 
             if prediction[0] == -1:
-                st.write("🔴 The URL is predicted to be suspicious.")
+                outcome_message= '🔴 The URL is predicted to be suspicious.'
             else:
-                st.write("🟢 The URL is predicted to be safe.")
+                outcome_message='🟢 The URL is predicted to be safe.'
+
+            st.write(outcome_message)
+
+            logger_info(f"Outcome for URL  {url} is  {outcome_message}")
 
             # Visualize the scaled data points
-            fig, ax = plt.subplots()
-            ax.scatter(data_scaled[:, 0], data_scaled[:, 1], c='green', edgecolors='black', marker='x', s=100)
-            ax.set_title("URL Prediction Visualization")
-            st.pyplot(fig)
+            # fig, ax = plt.subplots()
+            # ax.scatter(data_scaled[:, 0], data_scaled[:, 1], c='green', edgecolors='black', marker='x', s=100)
+            # ax.set_title("URL Prediction Visualization")
+            # st.pyplot(fig)
 
         except Exception as e:
             st.write(f"Error processing the URL: {e}")
 
 if __name__ == "__main__":
     main()
-
-# import pandas as pd
-# from features.get_domain_age import get_root_domain,get_domain_creation_date,days_passed_since
-# from features.get_page_rank import pagerank
-# from features.get_CAstatus import is_free_certificate
-# from features.get_domain_validity import get_validity_period
-# from src.get_domain import get_domain
-# from features.get_securitystatus import has_protective_statuses
-# from features.get_securitystatus2 import has_strong_security_headers
-# from features.get_catchingInfo_compressedinfo import check_caching_and_compression
-#
-# df = pd.read_csv("data/top-1m - Copy.csv")
-#
-# url = 'https://x.com/SimpleEnergyEV'
-# domain_age = []
-# page_rank = []
-# is_free_list = []
-# validation_period_list = []
-# has_protective_status_list = []
-# has_strong_security_list = []
-# caching_list = []
-# compressed_list = []
-# text_list = []
-# count = 0
-#
-#
-# domain = get_root_domain(url)
-# date_input = get_domain_creation_date(domain=domain)
-# age = days_passed_since(date_input=date_input)
-# domain_age.append(age)
-#
-# domain = get_domain(url)
-# rank = pagerank(domain=domain)
-# page_rank.append(rank)
-#
-# is_free = is_free_certificate(url)
-# is_free_list.append(is_free)
-#
-# validation_period = get_validity_period(url)
-# validation_period_list.append(validation_period)
-#
-# has_protective_status = has_protective_statuses(url)
-# has_protective_status_list.append(has_protective_status)
-#
-# has_strong_security = has_strong_security_headers(url)
-# has_strong_security_list.append(has_protective_status_list)
-#
-# caching,compressed = check_caching_and_compression(url)
-# caching_list.append(caching)
-# compressed_list.append(compressed)
-#
-# print("count is ----------->", count)
-# print("urls is -->", url)
-# print("age, pagerank-->", age, rank)
-# print("Is free CA-->",is_free,validation_period,has_protective_status, has_strong_security, caching,compressed) #,text)
-#
-# data = {
-#     "Url" : url,
-#     'Domain Age': domain_age,
-#     'Page Rank': page_rank,
-#     'Is Free': is_free_list,
-#     'Validation Period': validation_period_list,
-#     'Has Protective Status': has_protective_status_list,
-#     'Has Strong Security': has_strong_security_list,
-#     'Caching': caching_list,
-#     'Compressed': compressed_list,
-# }
-#
-# # Creating a DataFrame
-# df = pd.DataFrame(data)
-# df.to_csv(f"temp3/data{count}.csv")
-# count = count+1
